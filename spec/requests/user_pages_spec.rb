14651ca2dd4f0e00654c252fb940f216de09c815
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-19 00:42:24 +0530

    Creating tests for the 'flash' error and success messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -41,6 +48,13 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Example User') }
+        it { should have_content('Welcome to the Sample App!') }
       end
     end
   end
