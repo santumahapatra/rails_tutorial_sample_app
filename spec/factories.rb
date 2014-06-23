2af9b8d126998ea84f80f973ac472c6c7a77328a
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-23 16:03:51 +0530

    Addind the capability to add admin user to the factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobarbaz"
     password_confirmation "foobarbaz"
+
+    factory :admin do
+      admin true
+    end
   end
 end
