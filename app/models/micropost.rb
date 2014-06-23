fb1389870a012a9011b4bcea6ef45e67c70d2c41
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-24 00:05:59 +0530

    Returns micropost from the User.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,11 @@ class Micropost < ActiveRecord::Base
     followed_user_ids = user.followed_user_ids
     where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
   end
+
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
+  end
 end
